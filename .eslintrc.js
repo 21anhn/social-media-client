1a321444d2d6883bf2931540ebb9fd9159e999d8
21anhn <ltnanh21@gmail.com>
2024-11-17 16:43:36 +0700

    [Update] Update ESLint's configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,23 @@
+/* eslint-disable prettier/prettier */
 module.exports = {
   root: true,
   env: {
     node: true,
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
+    'plugin:vue/vue3-essential', // Cấu hình Vue3 cơ bản
+    'eslint:recommended', // Cấu hình các quy tắc cơ bản của ESLint
+    '@vue/typescript/recommended', // Cấu hình TypeScript cho Vue
+    'plugin:prettier/recommended', // Cấu hình Prettier cho ESLint
   ],
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2020, // Hỗ trợ ECMAScript 2020
+    parser: '@typescript-eslint/parser', // Sử dụng parser TypeScript
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off', // Cảnh báo khi có console trong môi trường production
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off', // Cảnh báo khi có debugger trong môi trường production
+    'vue/multi-word-component-names': ['error', { ignores: ['Login', 'Home'] }], // Quy tắc đa từ trong tên component, có thể bỏ qua cho các component phổ biến
+    'prettier/prettier': ['error', { endOfLine: 'lf' }], // Quy tắc của Prettier cho line breaks
   },
 };
